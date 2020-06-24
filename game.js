07906c0a27de7a585d6f06ccb727d4437369364f
Claudio Novoa <claudio.novoa.b@gmail.com>
2020-06-24 02:10:38 -0400

    working ok mvp

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
-const BASE_URL = 'https://minesweeper-rails-api.herokuapp.com';
+// const BASE_URL = 'https://minesweeper-rails-api.herokuapp.com';
 let GAME_URL;
-// const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3000';
 const grid = document.getElementById('game');
+const marquee = document.getElementById('marquee');
+let STATE;
 
 const newGame = (difficulty = 'beginner') => {
   axios.post(`${BASE_URL}/games`, { difficulty })
     .then(function(response) {
       const game = response.data;
       GAME_URL = `${BASE_URL}/games/${game.id}`;
-
+      STATE = game.state;
+      updateMarquee(game);
       for (let i = 0; i < game.rows; i++) {
         const row = grid.insertRow(i);
         for (let j = 0; j < game.cols; j++) {
@@ -20,6 +23,13 @@ const newGame = (difficulty = 'beginner') => {
     });
 };
 
+const updateMarquee = game => {
+  const flags = document.querySelectorAll('td.flagged').length;
+  marquee.rows[0].cells[0].innerHTML = game.mines - flags;
+  marquee.rows[0].cells[1].innerHTML = game.state;
+  marquee.rows[0].cells[2].innerHTML = 99;
+};
+
 const updateCell = (row, col, data) => {
   const cell = grid.rows[row].cells[col];
   cell.dataset.row = row;
@@ -28,18 +38,25 @@ const updateCell = (row, col, data) => {
   cell.dataset.flagged = data.flagged;
   cell.dataset.mine = data.mine;
   cell.dataset.value = data.value || '';
-  cell.innerHTML = data.value;
-  if (data.revealed && !cell.classList.contains('revealed')) {
-    cell.classList.add('revealed');
+  if (data.revealed) {
+    cell.innerHTML = data.value;
+    if(!cell.classList.contains('revealed')) {
+      cell.classList.add('revealed');
+    }
+    if (data.mine && !cell.classList.contains('mined')) {
+      cell.classList.add('mined');
+    }
   }
 };
 
 const updateGrid = (game) => {
+  STATE = game.state;
   for (let i = 0; i < game.rows; i++) {
     for (let j = 0; j < game.cols; j++) {
       updateCell(i, j, game.board[i][j]);
     }
   }
+  updateMarquee(game);
 };
 
 const revealCell = (row, col) => {
@@ -47,13 +64,7 @@ const revealCell = (row, col) => {
   axios.patch(`${GAME_URL}/board/${row}/${col}`, { revealed: true })
   .then((response) => {
     const game = response.data;
-    if (game.state === 'won') {
-      return alert('You win!');
-    } else if (game.state === 'lost') {
-      return alert('You lose :(');
-    } else {
-      updateGrid(response.data);
-    };
+    updateGrid(game);
   });
 };
 
@@ -62,27 +73,28 @@ const flagCell = (row, col) => {
   const flagged = grid.rows[row].cells[col].dataset.flagged === 'false';
   axios.patch(`${GAME_URL}/board/${row}/${col}`, { flagged })
   .then((response) => {
-    updateGrid(response.data);
     if (flagged) {
       cell.classList.add('flagged');
     } else {
       cell.classList.remove('flagged');
     }
+    updateGrid(response.data);
   });
 };
 
 grid.addEventListener('contextmenu', event => {
   event.preventDefault();
   if (event.target.className !== 'game-cell') return;
+  if (['won', 'lost'].includes(STATE)) return;
   const { row, col } = event.target.dataset;
   flagCell(row, col);
 });
 
 grid.addEventListener('click', event => {
-  if (event.target.className !== 'game-cell') return;
+  if (!event.target.classList.contains('game-cell')) return;
+  if (['won', 'lost'].includes(STATE)) return;
   const { row, col } = event.target.dataset;
   revealCell(row, col);
-  console.log('row', row, 'col', col);
 });
 
 newGame();
